54727829c7532114db16ed11fe604dce9b1884af
PedroCZuffo <pedro.cz2711@gmail.com>
2022-08-30 09:38:17 -0300

    Base da tela inicial

---
+++ rating_games/src/App.jsx
@@ -0,0 +1,24 @@
+import { Sidebar } from "./components/Sidebar";
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+
+import "./global.css"
+
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div>
+      <Header />
+    
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ rating_games/src/components/Topbar.jsx
@@ -0,0 +1,8 @@
+import styles from './Topbar.module.css'
+
+export function Topbar() {
+  return (
+    <div className={styles.topbar}>
+    </div>
+  );
+}
